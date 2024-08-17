f0f1385e618eb8b90de2a11d709b646f6f332e31
Aarobh-Kumar <165626806+aarobhs087@users.noreply.github.com>
2024-08-17 18:47:48 +0530

    Create solution.java

---
+++ GFG/Product array puzzle/solution.java
@@ -0,0 +1,28 @@
+// User function Template for Java
+
+class Solution {
+    public static long[] productExceptSelf(int nums[]) {
+        // code here
+        int n=nums.length;
+        long suff[]=new long[n];
+        long pref[]=new long[n];
+        long mul=1;
+        for(int i=0;i<n;i++){
+            mul*=nums[i];
+            pref[i]=mul;
+        }
+        mul=1;
+        for(int i=n-1;i>=0;i--){
+            mul*=nums[i];
+            suff[i]=mul;
+        }
+        long arr[]=new long[n];
+        for(int i=0;i<n;i++){
+            long x=1;
+            if(i>0) x=x*pref[i-1];
+            if(i<n-1)   x=x*suff[i+1];
+            arr[i]=x;
+        }
+        return arr;
+    }
+}
