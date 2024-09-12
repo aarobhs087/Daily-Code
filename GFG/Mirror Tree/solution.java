1062b50684d3da28ea4edda7de16e3b0785adb49
Aarobh-Kumar <165626806+aarobhs087@users.noreply.github.com>
2024-09-13 04:44:39 +0530

    Create solution.java

---
+++ GFG/Mirror Tree/solution.java
@@ -0,0 +1,25 @@
+// FUNCTION CODE
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)
+   {
+        data = item;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node node) {
+        // Your code here
+        if(node==null)  return;
+        mirror(node.left);
+        mirror(node.right);
+        Node temp=node.left;
+        node.left=node.right;
+        node.right=temp;
+    }
+}
