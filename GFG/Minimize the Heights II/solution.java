2305d669b8559414f071318eddcaf27243d261c0
Aarobh-Kumar <165626806+aarobhs087@users.noreply.github.com>
2024-09-17 05:12:48 +0530

    Create solution.java

---
+++ GFG/Minimize the Heights II/solution.java
@@ -0,0 +1,33 @@
+// User function Template for Java
+
+class Solution {
+    int getMinDiff(int[] arr, int k) {
+        // code here
+        int n=arr.length;
+        Arrays.sort(arr);
+        // Maximum possible height difference
+        int ans = arr[n - 1] - arr[0];
+ 
+        int tempmin, tempmax;
+        tempmin = arr[0];
+        tempmax = arr[n - 1];
+ 
+        for (int i = 1; i < n; i++) {
+ 
+            // if on subtracting k we got negative then
+            // continue
+            if (arr[i] - k < 0)
+                continue;
+ 
+            // Minimum element when we add k to whole array
+            tempmin = Math.min(arr[0] + k, arr[i] - k);
+ 
+            // Maximum element when we subtract k from whole
+            // array
+            tempmax
+                = Math.max(arr[i - 1] + k, arr[n - 1] - k);
+            ans = Math.min(ans, tempmax - tempmin);
+        }
+        return ans;
+    }
+}
