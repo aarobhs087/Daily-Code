2e82b2a58e5ddd845fa20b89ce1e67dbce36dcd7
Aarobh-Kumar <165626806+aarobhs087@users.noreply.github.com>
2024-09-25 05:13:49 +0530

    Create solution.java

---
+++ GFG/Palindrome Linked List/solution.java
@@ -0,0 +1,40 @@
+/* Structure of class Node is
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}*/
+
+class Solution {
+    // Function to check whether the list is palindrome.
+    boolean isPalindrome(Node head) {
+        // Your code here
+        Node head2=reverse(head);
+        while(head!=null&&head2!=null){
+            if(head.data!=head2.data)   return false;
+            head=head.next;
+            head2=head2.next;
+        }
+        return true;
+        
+    }
+    Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return head;
+    }
+}
