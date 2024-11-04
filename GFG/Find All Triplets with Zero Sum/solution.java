f69d68e9b9dcf48419be2eec9b24bc5c0491798c
Aarobh-Kumar <165626806+aarobhs087@users.noreply.github.com>
2024-11-04 07:00:36 +0530

    Create solution.java

---
+++ GFG/Find All Triplets with Zero Sum/solution.java
@@ -0,0 +1,21 @@
+// User function Template for Java
+class Solution {
+    public List<List<Integer>> findTriplets(int[] arr) {
+        // Your code here
+        List<List<Integer>> ans=new ArrayList<>();
+        for(int i=0;i<arr.length-2;i++){
+            for(int j=i+1;j<arr.length-1;j++){
+                for(int k=j+1;k<arr.length;k++){
+                    if(arr[i]+arr[j]+arr[k]==0){
+                        List<Integer> l=new ArrayList<>();
+                        l.add(i);
+                        l.add(j);
+                        l.add(k);
+                        ans.add(l);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
