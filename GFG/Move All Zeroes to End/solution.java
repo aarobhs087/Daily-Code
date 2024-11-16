932fd9c3b43e011ebed9229dc25ba610142e58ff
Aarobh-Kumar <165626806+aarobhs087@users.noreply.github.com>
2024-11-16 09:21:48 +0530

    Create solution.java

---
+++ GFG/Move All Zeroes to End/solution.java
@@ -0,0 +1,14 @@
+// User function Template for Java
+
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        // code here
+        int n=arr.length;
+        int j=0;
+        for(int i=0;i<n;i++)
+            if(arr[i]!=0)
+                arr[j++]=arr[i];
+        while(j<n)
+            arr[j++]=0;
+    }
+}
