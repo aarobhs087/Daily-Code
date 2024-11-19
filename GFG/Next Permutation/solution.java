8e3f5dba6b212638a46e3c49cfe933b667b2df86
Aarobh-Kumar <165626806+aarobhs087@users.noreply.github.com>
2024-11-19 07:29:00 +0530

    Create solution.java

---
+++ GFG/Next Permutation/solution.java
@@ -0,0 +1,48 @@
+// User function Template for Java
+
+class Solution {
+    void nextPermutation(int[] arr) {
+        // code here
+        int n = arr.length;
+
+        // Find the pivot index
+        int pivot = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        // If pivot point does not exist, reverse the whole array
+        if (pivot == -1) {
+            reverse(arr, 0, n - 1);
+            return ;
+        }
+
+        // Find the element from the right that is greater than pivot
+        for (int i = n - 1; i > pivot; i--) {
+            if (arr[i] > arr[pivot]) {
+                swap(arr, i, pivot);
+                break;
+            }
+        }
+
+        // Reverse the elements from pivot + 1 to the end
+        reverse(arr, pivot + 1, n - 1);
+    }
+
+    // Helper method to reverse array
+    private static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            swap(arr, start++, end--);
+        }
+    }
+
+    // Helper method to swap two elements
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
