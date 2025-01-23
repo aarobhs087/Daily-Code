f2de9a1f7ce162e6f4dd8b610e9c4239bf83b40e
Aarobh-Kumar <165626806+aarobhs087@users.noreply.github.com>
2025-01-24 03:59:30 +0530

    Create solution.java

---
+++ GFG/Detect Loop in linked list/solution.java
@@ -0,0 +1,35 @@
+// User function template for JAVA
+
+/* Node is defined as
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+*/
+
+class Solution {
+    // Function to check if the linked list has a loop.
+    public static boolean detectLoop(Node head) {
+        // Add code here
+        // Fast and slow pointers initially points to the head
+        Node slow = head, fast = head;
+
+        // Loop that runs while fast and slow pointer are not
+        // null and not equal
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            // If fast and slow pointer points to the same node,
+            // then the cycle is detected
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
